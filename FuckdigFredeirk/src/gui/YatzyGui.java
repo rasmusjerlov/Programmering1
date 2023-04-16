1a45a381819002a317a361fd23a2fc3688e7139b
rasmusjerlov <rasmus.jerloev@gmail.com>
2023-04-16 17:13:29 +0200

    Latest update

---
+++ FuckdigFredeirk/src/gui/YatzyGui.java
@@ -0,0 +1,102 @@
+package gui;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import model.YatzyDice;
+
+import java.util.ArrayList;
+
+public class YatzyGui extends Application {
+    private YatzyDice dice = new YatzyDice();
+
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("Yatzy");
+        GridPane pane = new GridPane();
+        this.initContent(pane);
+
+        Scene scene = new Scene(pane);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    // -------------------------------------------------------------------------
+
+    // txfValues shows the face values of the 5 dice.
+    private final TextField[] txfValues = new TextField[5];
+    // cbxHolds shows the hold status of the 5 dice.
+    private final CheckBox[] cbxHolds = new CheckBox[5];
+    // txfResults shows the obtained results.
+    // For results not set yet, the possible result of 
+    // the actual face values of the 5 dice are shown.
+    private final ArrayList<TextField> txfResults = new ArrayList<>(15);
+    // Shows points in sums, bonus and total.
+    private final TextField txfSumSame = new TextField();
+    private final TextField txfBonus = new TextField();
+    private final TextField txfSumOther = new TextField();
+    private final TextField txfTotal = new TextField();
+
+    private final Label lblThrowCount = new Label();
+    private final Button btnThrow = new Button(" Throw ");
+
+    private void initContent(GridPane pane) {
+        pane.setGridLinesVisible(false);
+        pane.setPadding(new Insets(10));
+        pane.setHgap(10);
+        pane.setVgap(10);
+
+        // ---------------------------------------------------------------------
+
+        GridPane dicePane = new GridPane();
+        pane.add(dicePane, 0, 0);
+        dicePane.setGridLinesVisible(false);
+        dicePane.setPadding(new Insets(10));
+        dicePane.setHgap(10);
+        dicePane.setVgap(10);
+        dicePane.setStyle("-fx-border-color: black");
+
+        // add txfValues, chbHolds
+        // TODO
+        // add lblThrowCount and btnThrow
+        // TODO
+
+        // ---------------------------------------------------------------------
+
+        GridPane scorePane = new GridPane();
+        pane.add(scorePane, 0, 1);
+        scorePane.setGridLinesVisible(false);
+        scorePane.setPadding(new Insets(10));
+        scorePane.setVgap(5);
+        scorePane.setHgap(10);
+        scorePane.setStyle("-fx-border-color: black");
+        int width = 50; // width of the text fields
+
+        // add labels for results
+        // add txfResults,
+        // TODO
+        // add labels and text fields for sums, bonus and total.
+        // TODO
+
+    }
+
+    // -------------------------------------------------------------------------
+
+    // Create an action method for btnThrow's action.
+    // Hint: Create small helper methods to be used in the action method.
+    // TODO
+
+    // -------------------------------------------------------------------------
+
+    // Create a method for mouse click on one of the text fields in txfResults.
+    // Hint: Create small helper methods to be used in the mouse click method.
+    // TODO
+
+}
